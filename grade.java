193d509bfc7b59ca9a747ca1536d29d60cb3e7e2
vikkash-EH <85560546+vikkash-EH@users.noreply.github.com>
2021-06-08 19:29:40 +0530

    Update grade.java

--- grade.java
+++ grade.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 /**
  *
  * @author vikkash
@@ -8,33 +8,8 @@ public class Mcjava {
 
     public static void main(String[] args) {
         
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter the total mark out of 100");
-        int mark= sc.nextInt();
-        if(mark>=90)
-        {
-            System.out.println("grade A");
-        }
-        else if(mark>=80)
-        {
-            System.out.println("grade B");
-        }
-        else if(mark>=70)
-        {
-            System.out.println("grade C");
-        }
-        else if(mark>=60)
-        {
-            System.out.println("grade D");
-        }
-        else if(mark>=50)
-        {
-            System.out.println("grade E");
-        }
-        else 
-        {
-            System.out.println("grade F");
-        }
+       
+        System.out.println("hello World");
         
         
     }
