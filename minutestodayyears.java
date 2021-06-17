a61fe38782d6df51c89bc2071de0e2de2eb722c3
vikkash-EH <85560546+vikkash-EH@users.noreply.github.com>
2021-06-17 19:15:32 +0530

    Update minutestodayyears.java

--- minutestodayyears.java
+++ minutestodayyears.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
 public class mtd {
     public void con(int min)
     {
-        double minutesInYear = 60 * 24 * 365;
+        int minutesInYear = 60 * 24 * 365;
         
     
-        long years = (long) (min / minutesInYear);
-        int days = (int) (min / 60 / 24) % 365;
+        int years =(min / minutesInYear);
+        int days =(min / 60 / 24) % 365;
 
         System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
     }
